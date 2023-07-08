bd0c8ed087aa3e71f8e5f24e8bff8b8c53b6a028
Harsh Singh <sharshn21@gmail.com>
2023-07-08 08:53:36 +0530

    Market-Wizard Modified Files

--- utils/data_ingestion.py
+++ utils/data_ingestion.py
@@ -25,21 +25,16 @@ class DataIngestion():
         self.api_key = api_key
     
     def get_data_set(self):
-        if f'{self.stock_code}.csv' not in list_files():
-            st.text('Using Generated Data from Tiingo')
-            config = {
-                'api_key': self.api_key,
-                'session': True
-            }
-            client = TiingoClient(config)
-            end_date =  get_today_date() # Specify the desired end date
-            start_date = end_date.replace(year=end_date.year - 10)  # Assuming 5 years of data
+        st.text('Using Generated Data from Tiingo')
+        config = {
+            'api_key': self.api_key,
+            'session': True
+        }
+        client = TiingoClient(config)
+        end_date =  get_today_date() # Specify the desired end date
+        start_date = end_date.replace(year=end_date.year - 10)  # Assuming 5 years of data
 
-            df = client.get_dataframe(self.stock_code, frequency='daily', startDate=start_date, endDate=end_date)
-            df.to_csv(f'./Datasets/{self.stock_code}.csv')
-            df = pd.read_csv(f'./Datasets/{self.stock_code}.csv', index_col='date', parse_dates=True)            
-            return df
-        
-        else:
-            st.text('Using Pre-used')
-            return pd.read_csv(f'./Datasets/{self.stock_code}.csv')
+        df = client.get_dataframe(self.stock_code, frequency='daily', startDate=start_date, endDate=end_date)
+        df.to_csv(f'./Datasets/{self.stock_code}.csv')
+        df = pd.read_csv(f'./Datasets/{self.stock_code}.csv', index_col='date', parse_dates=True)            
+        return df
